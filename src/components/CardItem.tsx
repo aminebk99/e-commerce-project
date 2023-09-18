f6302b64279e5687ab10c0696a7c57e0b403a97e
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-09-18 19:06:50 +0100

    add card

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,15 +1,16 @@
+
 import { Card, Button } from "react-bootstrap"
 
-const CardItem = () => {
+const CardItem = ({id, title, price, imgURL}) => {
+
     return (
         <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem', margin: '1rem'}}>
+                <Card.Img variant="top" src={imgURL} style={{objectFit: 'cover'}} height={"200px"}/>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>{title}</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        {price}
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
--- src/components/SlideCard.tsx
+++ src/components/SlideCard.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+
+import { Container, Row } from 'react-bootstrap'
 import CardItem from './CardItem'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
 
 const SlideCard = () => {
+
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        fecthProduct();
+    }, []);
+
+    const fecthProduct = async () => {
+        try {
+
+            const response = await axios.get("https://fakestoreapi.com/products");
+            setProduct(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <>
-            <Container 
-            style={{borderRadius: ".4rem"}}
-            className='shadow-sm p-3 bg-light'>
+            <Container
+                style={{ borderRadius: ".4rem" }}
+                className='shadow-sm p-3 bg-light'>
                 <h1>Flash Sale</h1>
-                <CardItem />
+                <Row>
+
+                    {product.map((product) => (
+                        <CardItem
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imgURL={product.image}
+                            price={product.price}
+                        />
+
+                    ))}
+
+                </Row>
+
+
 
             </Container>
         </>
