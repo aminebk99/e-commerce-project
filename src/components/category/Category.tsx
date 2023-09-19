f735e0e0e1463fd07b91ea2b49ee7200ffaca544
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-09-19 15:28:20 +0100

    add categories

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,38 +1,37 @@
-import { Container } from "react-bootstrap"
-import './category.css'
+import { Container } from "react-bootstrap";
+import './category.css';
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Category = () => {
-    const [category, setCategory] = useState([]);
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetchCatgories();
+        fetchCategories();
     }, []);
 
-    const fetchCatgories = async() =>{
+    const fetchCategories = async () => {
         try {
             const response = await axios.get("https://fakestoreapi.com/products/categories");
-            setCategory(response.data);
+            setCategories(response.data);
         } catch (error) {
             console.error(error);
         }
-    }
+    };
+
     return (
         <>
             <Container className="p-0 mb-4 d-flex justify-content-between">
-                {category.map((category) => (
-                    <div className="cards bg-light shadow-sm">
-                    <span>Icons</span>
-                    <h5>{category}</h5>
-                </div>
+                {categories.map((cat, index) => (
 
+                    <div key={index} className="cards bg-light shadow-sm">
+                        <span>Icons</span>
+                        <h5>{cat}</h5>
+                    </div>
                 ))}
-                
-                
             </Container>
         </>
-    )
-}
+    );
+};
 
-export default Category
+export default Category;
