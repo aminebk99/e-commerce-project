4365ea66f3c28bb981926c121f5e4f70019d2eae
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-09-19 14:55:00 +0100

    deploy

--- src/components/SlideCard.tsx
+++ src/components/SlideCard.tsx
@@ -24,7 +24,7 @@ const SlideCard: React.FC = () => {
 
     const fetchProduct = async () => {
         try {
-            const response = await axios.get<Product[]>("https://fakestoreapi.com/products?limit=6");
+            const response = await axios.get<Product[]>("https://fakestoreapi.com/products?limit=5");
             setProduct(response.data);
         } catch (error) {
             console.error(error);
