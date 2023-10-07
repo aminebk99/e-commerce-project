427926d01f8b39552b41398d9215f0fa0cfb48b6
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-10-07 15:12:50 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,13 +15,13 @@ const Header = () => {
                 </Link>
             </div>
             <div className='search-bar' style={{ width: "50%" }}>
-                <Form.Select aria-label="Default select example" style={{ width: "50%" }}>
+                {/* <Form.Select aria-label="Default select example" style={{ width: "50%" }}>
                     <FontAwesomeIcon icon={faList} />
                     <option>Open this select menu</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
-                </Form.Select>
+                </Form.Select> */}
                 <input className='pl-2' type="search" placeholder='Search Here...' />
             </div>
             <div className='cart'>
