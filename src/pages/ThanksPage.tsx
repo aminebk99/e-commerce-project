cd2c7ef1b9a21f30dda3e6475bffee6f95376c11
Mohamed-Amine-Bazaou <143953485+Mohamed-Amine-Bazaou@users.noreply.github.com>
2023-10-09 13:41:44 +0100

    update

--- src/pages/ThanksPage.tsx
+++ src/pages/ThanksPage.tsx
@@ -1,9 +1,30 @@
+import { Button, Container, Row } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const ThanksPage = () => {
     return (
-        <div>
+        <>
+        <Container>
+            <div className="thanks-card">
+<h1>Thanks for your order!</h1>
+<span>Thanks for placing order "number of order" </span>
+<span>We will send you a notification within 2days when it ships.</span>
+<p>
+    Get in touch with us if you have any question or concerns.
+</p>
+<Row>
+    <Link to={"/products"}>
+    <Button>Go to back shopping</Button>
+    </Link>
+    <Link to={"/tracking"}>
+        <Button>Tracking order</Button>
+    </Link>
+</Row>
+            </div>
+            
+        </Container>
 
-        </div>
+        </>
     )
 }
 
