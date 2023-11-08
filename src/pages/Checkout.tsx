8577d0ca9b51e9947c7c2153fdf597b27be0cb46
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-11-08 15:32:01 +0100

    set checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,8 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom"
 
 const Checkout = () => {
+    const[product, setProduct] = useState({});
     const { id } = useParams();
+    useEffect(() =>{
+        fetchProduct()
+    }, [id])
+    const fetchProduct = async ()=>{
+        try {
+            const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+            setProduct(response.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <Container>
             <Row>
@@ -12,6 +26,10 @@ const Checkout = () => {
                     <Row>
                         <Col>
                             <p>Contact information</p>
+                            <span>change</span>
+                        </Col>
+                        <Col>
+                            <p>{product.title}</p>
                             <span>change</span>
                         </Col>
 
