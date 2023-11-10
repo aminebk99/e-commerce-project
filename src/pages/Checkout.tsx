e0b50eb2b0f63f17fa40025b8343325d252164a6
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-11-10 11:06:09 +0100

    update checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -18,6 +18,8 @@ interface Product {
 const Checkout = () => {
     const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
+    // const [tax, setTax] = useState(10);
+    // const [shipping, setShipping] = useState(10);
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -30,9 +32,10 @@ const Checkout = () => {
         };
         fetchProduct();
     }, [id]);
+    // const total = (tax + shipping + product?.price).toFixed(2);
 
     return (
-        <Container>
+        <Container className="p-0">
             <Row >
                 <Col xl={2}>
                     <h3>Checkout</h3>
@@ -67,7 +70,6 @@ const Checkout = () => {
                                 <h4>{product?.price}</h4>
                             </div>
 
-
                             <div className="d-flex align-items-center mt-4 mb-4 justify-content-between">
                                 <div className="d-flex align-items-center">
                                     <img src={product?.image} width={'50px'} alt={product?.image} />
@@ -77,6 +79,38 @@ const Checkout = () => {
                                     </div>
                                 </div>
                                 <h4>{product?.price}</h4>
+                            </div>
+                            <hr className="border" />
+                            <div className="mb-4 mt-4 d-flex align-items-center justify-content-center">
+                                <input
+                                    type="text"
+                                    style={{ height: '3.2rem', outline: 'none' }}
+                                    placeholder="Enter gift code or discount code"
+                                    className="rounded w-100 border p-2"
+                                />
+
+                            </div>
+                            <hr className="border" />
+                            <div className="mt-2">
+                                <h3>Shopping summary</h3>
+                                <Row className="w-100 mt-4 d-flex flex-column">
+                                    <Col className="d-flex justify-content-between mb-2">
+                                        <span>Subtotal</span>
+                                        <span>${product?.price}</span>
+                                    </Col>
+                                    <Col className="d-flex justify-content-between mb-2">
+                                        <span>Tax</span>
+                                        <span>$0</span>
+                                    </Col>
+                                    <Col className="d-flex justify-content-between mb-2">
+                                        <span>Shipping</span>
+                                        <span>$0</span>
+                                    </Col>
+                                    <Col className="d-flex justify-content-between mt-4 mb-2">
+                                        <span><h4>Total</h4></span>
+                                        <span>${product?.price}</span>
+                                    </Col>
+                                </Row>
                             </div>
 
                         </div>
